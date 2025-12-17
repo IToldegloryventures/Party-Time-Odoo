a5502623cbe33a09d82d12d39421de37b7a5510c
amedina <ashton@partytimetexas.com>
2025-12-17 13:29:31 -0600

    Add missing author and license keys to awesome_owl manifest

--- addons/awesome_owl/__manifest__.py
+++ addons/awesome_owl/__manifest__.py
@@ -3,6 +3,8 @@
     "version": "19.0.1.0.0",
     "summary": "Minimal Owl playground for training exercises.",
     "category": "Hidden/Tools",
+    "author": "Party Time Texas",
+    "license": "LGPL-3",
     "depends": ["web"],
     "data": [
         "views/awesome_owl_templates.xml",
