b8a27178496be6cb0baa72df1a07220d152504f4
amedina <ashton@partytimetexas.com>
2025-12-29 15:16:08 -0600

    chore: Increment version to 19.0.1.0.7 for Enhancement #1 fixes

--- addons/ptt_business_core/__manifest__.py
+++ addons/ptt_business_core/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "PTT Business Core",
-    "version": "19.0.1.0.6",
+    "version": "19.0.1.0.7",
     "summary": "Party Time Texas core customizations for Contacts, CRM and Projects.",
     "category": "Customizations",
     "author": "Party Time Texas",
