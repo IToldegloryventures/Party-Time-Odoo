a85b9caccfae3c6211f124efb6b78e0b032fe3ae
Ashton Medina <ashtonmedina22@gmail.com>
2026-01-21 00:17:27 -0600

    fix(ptt_dashboard): fix duplicate class attributes in XML templates

--- addons/ptt_dashboard/static/src/components/KPILayouts/KpiLayoutOne/KpiLayoutOne.js
+++ addons/ptt_dashboard/static/src/components/KPILayouts/KpiLayoutOne/KpiLayoutOne.js
@@ -1,6 +1,6 @@
 /** @odoo-module **/
 
-import { Component, onMounted, useEffect, useState } from "@odoo/owl";
+import { Component, onMounted, useEffect, useState, markup } from "@odoo/owl";
 import { useService } from "@web/core/utils/hooks";
 
 export class KpiLayoutOne extends Component {
@@ -12,12 +12,14 @@ export class KpiLayoutOne extends Component {
   setup() {
     this.state = useState({
       data: this.props.data,
-      kpi_icon: this.props.data.kpi_icon || "",
+      kpi_icon: "",
     });
     useEffect(
       () => {
         this.state.data = this.props.data;
-        this.state.kpi_icon = this.props.data.kpi_icon || "";
+        if (this.state.data && this.state.data.default_icon) {
+          this.state.kpi_icon = markup(this.state.data.default_icon);
+        }
       },
       () => [this.props.data],
     );
