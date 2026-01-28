6aadc0297e1c2e69f6cd9e01ef3a6883837db3bd
Ashton Medina <ashtonmedina22@gmail.com>
2026-01-28 12:42:01 -0600

    Add ptt_event_type_id field to project.project in ptt_enhanced_sales
    The field was removed from ptt_business_core (which doesn't know about sale.order.type). Added it properly in ptt_enhanced_sales which defines the model and field. Also added project view extension to show the field.

--- addons/ptt_enhanced_sales/__manifest__.py
+++ addons/ptt_enhanced_sales/__manifest__.py
@@ -33,6 +33,7 @@ Designed specifically for Party Time Texas event management workflow.
         "views/sale_order_type_views.xml",
         "views/sale_order_views.xml",
         "views/crm_lead_views.xml",
+        "views/project_project_views.xml",
         "report/sale_order_report_templates.xml",
         "views/portal_sale_order_client_initials.xml",
     ],
--- addons/ptt_enhanced_sales/models/project_project.py
+++ addons/ptt_enhanced_sales/models/project_project.py
@@ -1,11 +1,21 @@
 # Part of Party Time Texas Event Management System
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
-from odoo import models
+from odoo import models, fields
 
 
 class ProjectProject(models.Model):
     """Extend project with event type link from sale.order.type."""
     _inherit = 'project.project'
 
-    # Field is defined in ptt_business_core as a related; no extra logic needed here.
+    # Event Type - defined here because sale.order.type is in ptt_enhanced_sales
+    # This field links to the CRM lead's event type via the related chain
+    ptt_event_type_id = fields.Many2one(
+        "sale.order.type",
+        related="ptt_crm_lead_id.ptt_event_type_id",
+        string="Event Type",
+        store=True,
+        readonly=False,
+        depends=['ptt_crm_lead_id'],
+        help="Event type template from the linked CRM opportunity"
+    )
